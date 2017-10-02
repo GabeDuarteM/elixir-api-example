7d87cc38ac49d7c9b0020af5635b4c075fe63198
Gabriel Duarte <gabrielqduarte@gmail.com>
2017-10-02 00:21:43 -0300

    init

---
+++ backend/web/controllers/user_controller.ex
@@ -0,0 +1,25 @@
+defmodule ApiExample.UserController do
+  use ApiExample.Web, :controller
+
+  def index(conn, _params) do
+      users = Repo.all(ApiExample.User) 
+
+      json conn_with_status(conn, users), users 
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(ApiExample.User, String.to_integer(id))
+
+    json conn_with_status(conn, user), user
+  end
+  
+  defp conn_with_status(conn, nil) do
+    conn
+      |> put_status(:not_found)
+  end
+  
+  defp conn_with_status(conn, _) do
+    conn
+      |> put_status(:ok)
+  end
+end
